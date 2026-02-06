e4c920fbc44a67e3d947214b9a6d635cd7934525
Joe Oakes <joe.oakes@gmail.com>
2026-02-05 19:49:44 -0500

    Prof Oakes help

---
+++ javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"Brakes"},{"p":"<Unnamed>","l":"Engine"},{"p":"<Unnamed>","l":"Gas"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
